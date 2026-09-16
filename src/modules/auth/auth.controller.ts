f43944cf9d700da6e9ea975920619671a9ae2586
Asadul Islam <asadulcb45@email.com>
2026-09-16 16:35:49 +0600

    refactor: wrap auth controllers with catchAsync

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status-codes";
+
 import { apiResponse } from "../../utils/apiResponse.ts";
+import { catchAsync } from "../../utils/catchAsync.js";
+
 import { authService } from "./auth.service.ts";
 
-const RegisterUser = async (req: Request, res: Response) => {
+const RegisterUser = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
+
   await authService.RegisterUser(payload);
 
   apiResponse(res, {
@@ -13,10 +17,11 @@ const RegisterUser = async (req: Request, res: Response) => {
     message: "please verify your email with the OTP sent to your email address",
     data: null,
   });
-};
+});
 
-const VerifyUser = async (req: Request, res: Response) => {
+const VerifyUser = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
+
   const { user, accessToken, refreshToken } =
     await authService.VerifyUser(payload);
 
@@ -24,49 +29,59 @@ const VerifyUser = async (req: Request, res: Response) => {
     httpOnly: true,
     secure: false,
     sameSite: "none",
-    maxAge: 1000 * 60 * 60 * 24, // 24 hour or 1 day
+    maxAge: 1000 * 60 * 60 * 24,
   });
+
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: false,
     sameSite: "none",
-    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    maxAge: 1000 * 60 * 60 * 24 * 7,
   });
 
   apiResponse(res, {
     success: true,
     statusCode: httpStatus.CREATED,
     message: "User Registered successfully",
-    data: { user, accessToken, refreshToken },
+    data: {
+      user,
+      accessToken,
+      refreshToken,
+    },
   });
-};
+});
 
-const LoginUser = async (req: Request, res: Response) => {
+const LoginUser = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
+
   const { accessToken, refreshToken } = await authService.LoginUser(payload);
 
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: false,
     sameSite: "none",
-    maxAge: 1000 * 60 * 60 * 24, // 24 hour or 1 day
+    maxAge: 1000 * 60 * 60 * 24,
   });
+
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: false,
     sameSite: "none",
-    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    maxAge: 1000 * 60 * 60 * 24 * 7,
   });
 
   apiResponse(res, {
     success: true,
     statusCode: httpStatus.OK,
     message: "User Logged in successfully",
-    data: { accessToken, refreshToken },
+    data: {
+      accessToken,
+      refreshToken,
+    },
   });
-};
+});
 
-const refreshToken = async (req: Request, res: Response) => {
+const refreshToken = catchAsync(async (req: Request, res: Response) => {
   const { refreshToken } = req.cookies;
 
   const result = await authService.refreshToken(refreshToken);
@@ -75,14 +90,14 @@ const refreshToken = async (req: Request, res: Response) => {
     httpOnly: true,
     secure: false,
     sameSite: "none",
-    maxAge: 1000 * 60 * 60 * 24, // 24 hour or 1 day
+    maxAge: 1000 * 60 * 60 * 24,
   });
 
   res.cookie("refreshToken", result.refreshToken, {
     httpOnly: true,
     secure: false,
     sameSite: "none",
-    maxAge: 1000 * 60 * 60 * 24 * 7, //  7 day
+    maxAge: 1000 * 60 * 60 * 24 * 7,
   });
 
   apiResponse(res, {
@@ -91,24 +106,25 @@ const refreshToken = async (req: Request, res: Response) => {
     message: "Token Generated successfully",
     data: result,
   });
-};
+});
 
-const googleLogin = async (req: Request, res: Response) => {
-  const Payload = req.body;
-  const result = await authService.googleLogin(Payload);
+const googleLogin = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+
+  const result = await authService.googleLogin(payload);
 
   res.cookie("accessToken", result.accessToken, {
     httpOnly: true,
     secure: false,
     sameSite: "none",
-    maxAge: 1000 * 60 * 60 * 24, // 24 hour or 1 day
+    maxAge: 1000 * 60 * 60 * 24,
   });
 
   res.cookie("refreshToken", result.refreshToken, {
     httpOnly: true,
     secure: false,
     sameSite: "none",
-    maxAge: 1000 * 60 * 60 * 24 * 7, //  7 day
+    maxAge: 1000 * 60 * 60 * 24 * 7,
   });
 
   apiResponse(res, {
@@ -117,19 +133,21 @@ const googleLogin = async (req: Request, res: Response) => {
     message: "User Logged in successfully",
     data: result,
   });
-};
+});
 
-const logoutUser = async (req: Request, res: Response) => {
+const logoutUser = catchAsync(async (req: Request, res: Response) => {
   await authService.logoutUser();
+
   res.clearCookie("accessToken");
   res.clearCookie("refreshToken");
+
   apiResponse(res, {
     success: true,
     statusCode: httpStatus.OK,
     message: "User Logged out successfully",
     data: null,
   });
-};
+});
 
 export const authController = {
   RegisterUser,
